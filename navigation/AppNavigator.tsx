7c83ac9540cacdbc198f94b8cd8cc7c14495db1c
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-08-22 16:47:11 +0200

    Feat(Navigation): Updated Navigation
    Updated Navigation to include profileScreen, OnboardingScreen and TripsInsightScreen.
    Removed Login and Register Screen as more logic applied within the App.tsx for different states and use cases such as logged in and closing app or logged out and closing app

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TripScreen from '../screens/TripScreen';
-import TimerScreen from '../screens/TimerScreen.tsx';
+import TimerScreen from '../screens/TimerScreen';
 import PuzzleScreen from '../screens/PuzzleScreen'; 
 import SuccessScreen from '../screens/SuccessScreen'; 
 import ETAShareScreen from '../screens/ETAShareScreen';
@@ -9,6 +9,10 @@ import HomeScreen from '../screens/HomeScreen';
 import FriendsScreen from '../screens/FriendsScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
+import OnboardingScreen from 'screens/OnboardingScreen';
+import ProfileScreen from 'screens/ProfileScreen';
+import TripsInsightScreen from 'screens/TripsInsightScreen';
+
 
 
 const Stack = createNativeStackNavigator();
@@ -23,8 +27,11 @@ export default function AppNavigator() {
       <Stack.Screen name="SuccessScreen" component={SuccessScreen} />
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
       <Stack.Screen name="FriendsScreen" component={FriendsScreen} />
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      {/* <Stack.Screen name="LoginScreen" component={LoginScreen} /> */}
+      {/* <Stack.Screen name="RegisterScreen" component={RegisterScreen} /> */}
+      <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+      <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
+      <Stack.Screen name="TripsInsightScreen" component={TripsInsightScreen} />
     </Stack.Navigator>
   );
 }
